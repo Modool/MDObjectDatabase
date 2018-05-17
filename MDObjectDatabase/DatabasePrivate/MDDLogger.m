210516837fde438cb3068fda0ade5e8e61890ac6
xulinfeng <xulinfeng@bilibili.com>
2018-05-17 12:37:02 +0800

    Fixed issues.

--- MDObjectDatabase/DatabasePrivate/MDDLogger.m
+++ MDObjectDatabase/DatabasePrivate/MDDLogger.m
@@ -8,34 +8,44 @@
 
 #import "MDDLogger.h"
 
+#define dispatch_main_async_safe(block)     \
+{   if ([NSThread isMainThread]) block();   \
+    else dispatch_async(dispatch_get_main_queue(), block);  \
+}
+
 @implementation MDDLogger
 
 - (void)log:(NSString *)format, ...{
     if (!self.enable) return;
     
     va_list list;
-    va_list copiedList;
     va_start(list, format);
-    va_copy(copiedList, list);
     va_end(list);
     
-    [self logLevel:MDDLoggerLevelDebug format:format args:copiedList];
+    format = [self format:format level:MDDLoggerLevelDebug];
+    NSString *log = [[NSString alloc] initWithFormat:format arguments:list];
+    
+    dispatch_main_async_safe(^{
+        NSLog(@"%@", log);
+    });
 }
 
 - (void)logLevel:(MDDLoggerLevel)level format:(NSString *)format, ...{
     if (!self.enable) return;
     
     va_list list;
-    va_list copiedList;
     va_start(list, format);
-    va_copy(copiedList, list);
     va_end(list);
     
-    [self logLevel:level format:format args:copiedList];
+    format = [self format:format level:level];
+    NSString *log = [[NSString alloc] initWithFormat:format arguments:list];
+    
+    dispatch_main_async_safe(^{
+        NSLog(@"%@", log);
+    });
 }
 
-- (void)logLevel:(MDDLoggerLevel)level format:(NSString *)format args:(va_list)args{
-    if (!self.enable) return;
+- (NSString *)format:(NSString *)format level:(MDDLoggerLevel)level{
     static NSDictionary *levelDescriptions = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -45,15 +55,7 @@
                               @(MDDLoggerLevelError): @"ERROR"};
     });
     
-    format = [NSString stringWithFormat:@"%@: %@", levelDescriptions[@(level)], format];
-
-    if ([NSThread isMainThread]) {
-        NSLogv(format, args);
-    } else {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            NSLogv(format, args);
-        });
-    }
+    return [NSString stringWithFormat:@"%@: %@", levelDescriptions[@(level)], format];
 }
 
 @end
